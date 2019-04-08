a21aed049ad0c68e3d8f9f3afb23304e5ae17ec1
Sphere <2257930660@qq.com>
2019-04-08 18:27:34 +0800

    test

--- src/UI/test.java
+++ src/UI/test.java
@@ -1,21 +1,27 @@
 package UI;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
+import Components.Trigger;
 import Depends.Vector;
+import Prefabs.Prefab;
 
 public class test {
-
+	
 	public test() {
-		// TODO Auto-generated constructor stub
+		Vector vector1=new Vector(0,100);
+		Vector vector2=new Vector(0,100);
+		double cos=Vector.getCosAngle(vector1, vector2);
+		System.out.println(cos);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Double a=100.;
-		System.out.println(a);
-		change(a);
-		System.out.println(a);
+		new test();ss
 	}
-	public static void change(Double d) {
-		d+=10;
-	}
+	
 }
+
